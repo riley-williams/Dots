47a0e76f9c2f2e03ebfbd8c3abdbdf4178e03f80
Riley Williams <williams.riley@me.com>
2014-08-23 16:38:56 -0400

    Initial Commit

---
+++ Dots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dots
+//
+//  Created by Riley Williams on 8/23/14.
+//  Copyright (c) 2014 Riley Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
